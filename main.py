ad561fc5b9e738cb8ce404400883429fea7a703b
Ioplop <ishk909@gmail.com>
2024-11-23 12:05:26 -0300

    Add basic pygame screen loop with quit option

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pygame
+
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+W, H = screen.get_size()
+background_color = (20, 20, 50)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+    screen.fill(background_color)
+    pygame.display.flip()
+
+pygame.quit()
