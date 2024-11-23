9986c549b1af4bc70bad82da1ff23a59e3259a2c
Ioplop <ishk909@gmail.com>
2024-11-23 12:43:08 -0300

    Added basic sand fall function called simulate()

--- world.py
+++ world.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import Tuple, List, Set
+from random import random
 
 
 class World:
@@ -43,6 +44,28 @@ class World:
         sand.x = x
         sand.y = y
         self.grid[x][y] = sand
+    
+    def get_sand(self, x: int, y: int) -> Sand:
+        """Find the sand object at a specific position and returns it.
+        Loops around vertical edges.
+        Doesn't loop on horizontal edges. Returns "OUTSIDE" instead.
+
+        Args:
+            x (int): X position of the sand.
+            y (int): Y position of the sand.
+
+        Returns:
+            Sand: Whatever is found.
+        """
+        while x < 0:
+            x += self.W
+        while x >= self.W:
+            x -= self.W
+        while y < 0:
+            return "OUTSIDE"
+        while y >= self.H:
+            return "OUTSIDE"
+        return self.grid[x][y]
         
 
 class Sand:
@@ -66,3 +89,32 @@ class Sand:
         """Removes a sand object from its world."""
         self.world.remove_sand(self)
     
+    def simulate(self) -> Set[Sand]:
+        """Executes a simulation step for a sand object. Returns a set of all the affected sand surrounding it."""
+        affected = set()
+        below = self.world.get_sand(self.x, self.y-1)
+        fell = False
+        if below == "OUTSIDE":
+            return set()
+        if not below:
+            self.move(self.x, self.y-1)
+            fell = True
+        else:
+            l = self.world.get_sand(self.x-1, self.y-1)
+            r = self.world.get_sand(self.x+1, self.y-1)
+            xdown = 0
+            if not (l or r):
+                xdown = -1 if random < 0.5 else 1
+            elif not l:
+                xdown = -1
+            elif not r:
+                xdown = 1
+            if xdown:
+                fell = True
+                self.move(xdown, self.y-1)
+        if fell:        
+            affected.add(self.world.get_sand(self.x-1, self.y+1))
+            affected.add(self.world.get_sand(self.x, self.y+1))
+            affected.add(self.world.get_sand(self.x+1, self.y+1))
+        return affected
+        
